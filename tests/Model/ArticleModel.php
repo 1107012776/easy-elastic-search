aea0ae64f5d74cce43953e991e44af07038a34d0
linyushan <linyushan@nawang.cn>
2021-11-10 11:38:56 +0800

    格式化代码

--- tests/Model/ArticleModel.php
+++ tests/Model/ArticleModel.php
@@ -1,12 +1,15 @@
 <?php
+
 namespace PhpEasyData\Tests\Model;
+
 use PhpEasyData\Core\Model;
 use PhpEasyData\Core\TransactionTrait;
 use PhpEasyData\Driver\ElasticSearchDriver;
 use PhpEasyData\Inter\DriverInter;
 use PhpEasyData\Inter\TransactionInter;
 
-class ArticleModel extends Model implements DriverInter,TransactionInter {
+class ArticleModel extends Model implements DriverInter, TransactionInter
+{
     use TransactionTrait;
     protected $tableName = 'article';
     protected $driverClass = ElasticSearchDriver::class;
--- tests/Model/UserModel.php
+++ tests/Model/UserModel.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace PhpEasyData\Tests\Model;
+
 use PhpEasyData\Core\Model;
 use PhpEasyData\Driver\ElasticSearchDriver;
 use PhpEasyData\Inter\DriverInter;
 
-class UserModel extends Model implements DriverInter {
+class UserModel extends Model implements DriverInter
+{
     protected $tableName = 'user';
     protected $driverClass = ElasticSearchDriver::class;
 }
