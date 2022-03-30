0e235393daec2c9e701eb7c7284a466a1711b941
linyushan <linyushan@nawang.cn>
2022-03-30 14:53:20 +0800

    es封装代码修改

--- tests/Model/ArticleModel.php
+++ tests/Model/ArticleModel.php
@@ -3,14 +3,11 @@
 namespace PhpEasyData\Tests\Model;
 
 use PhpEasyData\Core\Model;
-use PhpEasyData\Core\TransactionTrait;
 use PhpEasyData\Driver\ElasticSearchDriver;
 use PhpEasyData\Inter\DriverInter;
-use PhpEasyData\Inter\TransactionInter;
 
-class ArticleModel extends Model implements DriverInter, TransactionInter
+class ArticleModel extends Model implements DriverInter
 {
-    use TransactionTrait;
     protected $tableName = 'article';
     protected $driverClass = ElasticSearchDriver::class;
 }
